5ab1d882fa4b47526219466b41e085c8893facb9
Mkzlbg <110672863+Mkzlbg@users.noreply.github.com>
2022-08-16 22:11:54 +0300

    Update C01Deneme.java

--- C01Deneme.java
+++ C01Deneme.java
@@ -2,5 +2,6 @@ public class C01Deneme {
     public static void main(String[] args) {
         System.out.println("ilk deneme class");
         System.out.println("2. deneme class");
+        System.out.println("3. deneme class");
     }
 }
